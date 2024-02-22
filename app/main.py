ee7897b18fdac74daeae11d1405bdd610ae30465
eselimnl <eselimnl@gmail.com>
2024-02-22 17:34:00 +0000

    preliminary structure of logs

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from src.utils import cache_load_gene_summary
 
 from src.app_interface import set_up_interface, file_selector
 from src.app_configs_menu import process_configs_menu
+from src.app_gene_facts import process_gene_facts
 from src.app_haplotype_plot import generate_haplotype_plot
 from src.app_abacus_plot import generate_abacus_plot
 
@@ -21,7 +22,9 @@ def main():
     df_haplotypes, df_join, background_ns_changes, _ = cache_load_gene_summary(filename)
     
     min_samples, sample_count_mode = process_configs_menu(gene_id_selected)
-    
+
+    process_gene_facts(gene_id_selected, job_logs_file="work/backend/job_logs.json")
+
     ns_changes, df_haplotypes_set = generate_haplotype_plot(df_haplotypes, gene_id_selected, background_ns_changes, min_samples, sample_count_mode)
     
     generate_abacus_plot(ns_changes, df_join, min_samples, df_haplotypes_set)
