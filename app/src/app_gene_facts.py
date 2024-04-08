1bd0d92f970d85210a9c74f403fe62e66464921b
eselimnl <eselimnl@gmail.com>
2024-04-08 16:04:48 +0100

    improvements based on PR feedback

--- app/src/app_gene_facts.py
+++ app/src/app_gene_facts.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import json
 from src.utils import cache_load_population_colours, _cache_load_utility_mappers
+import pandas as pd
 
-def process_gene_facts(gene_id_selected, job_logs_file="path/to/job_logs.json"):
+def process_gene_facts(min_samples, df_haplotypes, gene_id_selected, job_logs_file="path/to/job_logs.json"):
     """Main function called in main.py to show gene facts"""
     
     utility_mappers = _cache_load_utility_mappers()
@@ -25,19 +26,22 @@ def process_gene_facts(gene_id_selected, job_logs_file="path/to/job_logs.json"):
     col1, col2 = st.columns([3, 2])
     with col1.expander("Click to view how many Pf7 samples are included/excluded."):
         included_samples = gene_info.get('c_inc_s', 'N/A')
-        excluded_samples = 20864 - included_samples if included_samples != 'N/A' else 'N/A'
         qc_fail = 4661
         missing_genotype_calls = gene_info.get('c_missing', 'N/A')
         heterozygous_calls = gene_info.get('c_het_calls', 'N/A')
         stop_codons = gene_info.get('c_stop_codon', 'N/A')
-        st.write(f"**Included:** {included_samples} ({included_samples/20864*100:.2f}%)")
-        st.write(f"""
-            **Excluded:** {excluded_samples} ({excluded_samples/20864*100:.2f}%)
-            | Exclusion Reason           | Count      | Percentage |
-            |----------------------------|------------|------------|
-            | QC fail                    | {qc_fail}   | 22.34%     |
-            | Missing genotype calls     | {missing_genotype_calls} | {missing_genotype_calls/20864*100:.2f}% |
-            | Heterozygous calls         | {heterozygous_calls} | {heterozygous_calls/20864*100:.2f}% |
-            | Stop codons                | {stop_codons} | {stop_codons/20864*100:.2f}% |
-            | Total                 | {excluded_samples} | {excluded_samples/20864*100:.2f}% |
-        """)
+        sample_below_threshold = (df_haplotypes['Total'] < min_samples).sum()
+        excluded_samples = 20864 - included_samples + sample_below_threshold
+        # Create data for the table
+        table_data = [
+            {"Exclusion Reason": "QC fail", "Count": qc_fail, "Percentage": qc_fail / 20864 * 100},
+            {"Exclusion Reason": "Missing genotype calls", "Count": missing_genotype_calls, "Percentage": missing_genotype_calls / 20864 * 100},
+            {"Exclusion Reason": "Heterozygous calls", "Count": heterozygous_calls, "Percentage": heterozygous_calls / 20864 * 100},
+            {"Exclusion Reason": "Stop codons", "Count": stop_codons, "Percentage": stop_codons / 20864 * 100},
+            {"Exclusion Reason": f"Sample threshold ({min_samples})", "Count": sample_below_threshold, "Percentage": sample_below_threshold / 20864 * 100},
+            {"Exclusion Reason": "Total", "Count": excluded_samples, "Percentage": excluded_samples / 20864 * 100}
+        ]
+        df_table = pd.DataFrame.from_dict(table_data)
+        df_table['Percentage'] = df_table['Percentage'].apply(lambda x: '{:.2f}%'.format(x))
+        print(df_table)
+        st.table(df_table.set_index('Exclusion Reason', drop=True))
