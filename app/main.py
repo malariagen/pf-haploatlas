804c1b6aea6aff3573d7b30b46768d5f4a10d532
cl321 <cl32@sanger.ac.uk>
2024-04-20 00:40:32 +0100

    fixing savefile naming

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from src.utils import cache_load_gene_summary
 
 from src.app_interface import set_up_interface, file_selector
 from src.app_configs_menu import process_configs_menu
-# from src.app_gene_facts import process_gene_facts
 from src.app_haplotype_plot import generate_haplotype_plot
 from src.app_abacus_plot import generate_abacus_plot
 from app.src.app_worldmap_plot import generate_worldmap_plot
@@ -23,8 +22,6 @@ def main():
     df_haplotypes, df_join, background_ns_changes = cache_load_gene_summary(filename)
 
     min_samples, sample_count_mode = process_configs_menu(gene_id_selected, df_haplotypes, df_join)
-
-    # process_gene_facts(min_samples, df_haplotypes, df_join, gene_id_selected, job_logs_file="work/backend/job_logs.json")
 
     ns_changes, df_haplotypes_set = generate_haplotype_plot(df_haplotypes, gene_id_selected, background_ns_changes, min_samples, sample_count_mode)
     
