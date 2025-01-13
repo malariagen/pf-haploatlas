706b7c2f19544a31b3d24592461e7f76e5ee7356
Chiyun-Lee <chiyun.alberto.lee@gmail.com>
2025-01-13 18:10:25 +0000

    fixed call_haplotypes behaviour

--- app/src/utils.py
+++ app/src/utils.py
@@ -2,7 +2,7 @@ import streamlit as st
 import json, os, lzma, pickle, collections, io
 import pandas as pd
 
-DATAPACK_BASE_PATH = "app/files/datapack"
+DATAPACK_BASE_PATH = "app/files/datapack-2025-01-10"
 
 @st.cache_data
 def _cache_load_utility_mappers(base_path = DATAPACK_BASE_PATH):
@@ -150,7 +150,7 @@ def _st_justify_markdown_html(text: str, location = None):
 def _present_cookie_banner():
     tab1, tab2 = st.tabs(["Welcome", "Cookie details"])
 
-    tab1.markdown("We now use **MalariaGEN Pf8**, which means that the Pf-HaploAtlas now contains data from **24,410** QC-passed samples. That's _8,207_ more, or over _50%_ more than the last version. Happy haplotype hunting!")
+    tab1.markdown("We now use **MalariaGEN Pf8**, which means that the Pf-HaploAtlas now contains data from **24,409** QC-passed samples. That's _8,207_ more, or over _50%_ more than the last version. Happy haplotype hunting!")
 
     tab1.markdown("""<p style="text-align: right;"><i>- the HaploAtlas team</i></p>""", unsafe_allow_html = True)
 
