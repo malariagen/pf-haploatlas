9682811161f0b5e468b15a699d3a08ad8de3b935
Chiyun-Lee <chiyun.alberto.lee@gmail.com>
2026-02-13 15:22:26 +0000

    changed licensing and branding

--- app/src/app_interface.py
+++ app/src/app_interface.py
@@ -239,20 +239,11 @@ If you'd like to report a bug, request a feature, or give us feedback, check out
             [100]
         )
 
-        _show_images_with_urls(
-            ["app/files/logo_gsu.png", "app/files/logo_sanger.png"],
-            ["https://www.sanger.ac.uk/collaboration/genomic-surveillance-unit/", "https://www.sanger.ac.uk/"],
-            [130, 80],
-            [130, 80]
-        )
-
-        st.divider()
-
         st.markdown("## Funded by")
         _show_images_with_urls(
-            ["app/files/logo_bmgf.png"],
+            ["app/files/logo_gates.png"],
             ["https://www.gatesfoundation.org/"],
-            [60],
+            [70],
             [100]
         )
 
@@ -261,7 +252,7 @@ If you'd like to report a bug, request a feature, or give us feedback, check out
         st.markdown(
             """
 <div style="text-align:center">
-    Copyright © 2021 - 2024 Genome Research Ltd.
+    Copyright © MalariaGEN at the Liverpool School of Tropical Medicine.
 </div>
             """
         , unsafe_allow_html = True)
